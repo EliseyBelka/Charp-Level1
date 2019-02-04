72360c4a24b8d9a2f0086595bb1ab5457efdd120
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-02-04 17:57:13 +0700

    1 и 2 задание. Message ко 2 задаче
    Серегей в прошлом д\з написали почему выполняю мало заданий? 
    На данный момент на лечение. В связи с этим решаю задачи согласно условиям что бы было принято, времени мало.

---
+++ Task1.cs
@@ -0,0 +1,56 @@
+﻿//Создать программу, которая будет проверять корректность ввода логина.
+//Корректным логином будет строка от 2 до 10 символов, содержащая только 
+//буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+//а) без использования регулярных выражений;
+//АВТОР исполнения Белканов Алексей 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesons5
+{
+    class Task1
+    {
+        static void Pause()
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            Console.WriteLine("Press ESC to exit");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+
+        static void Main(string[] args)
+        {
+            string login;
+            int checksum=0;
+            Console.WriteLine("Enter a username between 2 to 20 characters");
+            login=Console.ReadLine();
+           //Console.WriteLine((int)login[0]);
+            if (login.Length <= 1 | login.Length >= 21)
+            {
+                Console.WriteLine("Error. Length of the login does not meet the conditions");
+            } else
+                    if ((int)login[0] >= 48 & (int)login[0] <= 57)
+                    Console.WriteLine("Error. Login should not start with a number.");
+                      else
+                    foreach (char ch in login)
+                    {
+                       // Console.WriteLine((int)ch);
+                        if ((int)ch >= 48 && (int)ch <= 57 | (int)ch >= 65 && (int)ch <= 90 | (int)ch >= 97 && (int)ch <= 122)
+                        {
+                        checksum++;
+                        } else
+                        {
+                        checksum--;
+                        }
+                    }
+            if (checksum == login.Length)
+                Console.WriteLine("Login is correct");
+            else
+                Console.WriteLine("Login is not correct");
+            Pause();
+        }
+        
+    }
+}
---
+++ Task2a-г.cs
@@ -0,0 +1,66 @@
+﻿//Разработать класс Message, содержащий следующие статические методы для обработки
+//текста:
+//а) Вывести только те слова сообщения, которые содержат не более n букв.
+//б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+//в) Найти самое длинное слово сообщения.
+//г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+//Продемонстрируйте работу программы на текстовом файле с вашей программой.
+//АВТОР исполнения Белканов Алексей
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesons5
+{
+    class Task2
+    {
+        static void Pause()
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            Console.WriteLine("Press ESC to exit");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+
+        static void Main(string[] args)
+        {
+            //Task-a
+            Console.WriteLine("Enter a test string");
+            string testa = (Console.ReadLine());
+            Console.WriteLine("What is the maximum length of words to output?");
+            int leng = int.Parse(Console.ReadLine());
+            //Вызов метода 
+            string[] resulta = (Message.SieveWords(testa, leng));
+            //Вывод массива строк переданных из метода
+            for (int i = 0; i < resulta.Length; i++)
+                Console.Write(resulta[i] + " ");
+
+            //Task-b
+            Console.WriteLine("Enter a test string");
+            string testb = (Console.ReadLine());
+            Console.WriteLine("Which last character in the word should I delete?");
+            char charb = Convert.ToChar(Console.ReadLine());
+            //Вызов метода 
+            string[] resultb = (Message.DeleteWordEndChar(testb, charb));
+            //Вывод массива строк переданных из метода
+            for (int i = 0; i < resultb.Length; i++)
+                Console.Write(resultb[i] + " ");
+
+            //Task-с
+            Console.WriteLine("Enter a test string");
+            string testс = (Console.ReadLine());
+            //Вызов метода 
+            Console.WriteLine(Message.LongestWord(testс));
+
+            //Task-d
+            Console.WriteLine("Enter a test string");
+            string testd = (Console.ReadLine());
+            Console.WriteLine("What is the size of the string is considered large?");
+            int lengd = int.Parse(Console.ReadLine());
+            //Вызов метода 
+            Console.WriteLine(Message.SBString(testd,lengd));
+            Pause();
+        }
+    }
+}
