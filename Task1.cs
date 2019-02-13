dc2770878be880f8fe93af4db9dc5eb21c266916
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-02-13 10:35:25 +0700

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,23 @@
+﻿//С помощью рефлексии выведите все свойства структуры DateTime
+//Автор Алексей Белканов
+using System;
+using System.Reflection;
+namespace cshLesson8
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            DateTime dateTime = new DateTime();
+            Console.WriteLine("All properties of a class DataTime:");
+            foreach (var prop in dateTime.GetType().GetProperties())
+            {
+                i++;
+                Console.WriteLine("{0}.{1}",i, prop.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
